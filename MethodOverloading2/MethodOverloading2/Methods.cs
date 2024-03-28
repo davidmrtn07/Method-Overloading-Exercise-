84d5f9e6a55363fdb079e6c46027d76f32531fdb
davidmrtn07 <davidmrtn07@gmail.com>
2024-03-27 20:08:36 -0500

    Overlod Try 1

---
+++ MethodOverloading2/MethodOverloading2/Methods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading2
+{
+    internal class Methods
+    {//create add method
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+     //create an overload of the method that adds two decimals 
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+        public static string Add(int x, int y, bool withDollars)
+        {
+            if(withDollars && (x + y) != 1)
+            {
+                return $"{x + y} dollars";
+            }
+            else if(withDollars && (x + y) == 1)
+            {
+                return $"{x + y} dollar";
+            }
+            else
+            {
+                return $"{x + y}";
+            }
+        }
+    }
+
+}
---
+++ MethodOverloading2/MethodOverloading2/Program.cs
@@ -0,0 +1,6 @@
+﻿
+
+using MethodOverloading2;
+
+var answer = Methods.Add(2, 2);
+Console.WriteLine(answer);
